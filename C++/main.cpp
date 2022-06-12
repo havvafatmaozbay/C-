e76f6850d318ee2ab881cedb19ddaf80c944674b
Havva Fatma Özbay <havvafatma45@gmail.com>
2022-06-12 18:56:29 +0300

    Initial Commit

---
+++ C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++
+//
+//  Created by Havva Fatma Özbay on 12.06.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
